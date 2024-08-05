f3ada3837be595ebaac377a702bdf383c890cde1
aaguirre <alejandra36a@gmail.com>
2024-08-05 11:38:04 +0000

    cambiosss

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,25 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 
 
-const TrafficLight = () =>{
-    const [color, setColor]= useState("red");
+const TrafficLight = () => {
+    const [color, setColor] = useState("red");
 
     return (
-        <>
-        <div className="palo"></div>
-        <div className="semaforo">
-			<div 
-                className={`light ${color === 'red' ? 'red activo' : 'red'}`} 
-                onClick={() => setColor('red')}
-            ></div>
-           
-			<div className={`light ${color === 'yellow' ? 'yellow activo' : 'yellow'}`} 
-                onClick={() => setColor('yellow')}></div>
-			<div className={`light ${color === 'green' ? 'green activo' : 'green'}`} 
-                onClick={() => setColor('green')}></div>
-		</div>
-        </>
+        <div className="contenedor">
+            <div className="palo"></div>
+            <div className="semaforo">
+                <div
+                    className={`light ${color === 'red' ? 'red activo' : 'red'}`}
+                    onClick={() => setColor('red')}
+                ></div>
+
+                <div className={`light ${color === 'yellow' ? 'yellow activo' : 'yellow'}`}
+                    onClick={() => setColor('yellow')}></div>
+                <div className={`light ${color === 'green' ? 'green activo' : 'green'}`}
+                    onClick={() => setColor('green')}></div>
+            </div>
+        </div>
 
     );
 }
