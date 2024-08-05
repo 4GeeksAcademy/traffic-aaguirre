e5b867e7811c8a632f2cb9d2559a2a5dc0faf458
aaguirre <alejandra36a@gmail.com>
2024-08-05 10:50:10 +0000

    cambios primero

---
+++ src/js/component/trafficLight.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+
+
+
+const TrafficLight = () =>{
+    const [color, setColor]= useState("red");
+
+    return (
+        <>
+        <div className="palo"></div>
+        <div className="semaforo">
+			<div 
+                className={`light ${color === 'red' ? 'red activo' : 'red'}`} 
+                onClick={() => setColor('red')}
+            ></div>
+           
+			<div className={`light ${color === 'yellow' ? 'yellow activo' : 'yellow'}`} 
+                onClick={() => setColor('yellow')}></div>
+			<div className={`light ${color === 'green' ? 'green activo' : 'green'}`} 
+                onClick={() => setColor('green')}></div>
+		</div>
+        </>
+
+    );
+}
+
+export default TrafficLight;
