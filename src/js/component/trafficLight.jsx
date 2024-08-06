291ffe5cd09aebe8e0eb8cfa3f5111f56e2ff49e
aaguirre <alejandra36a@gmail.com>
2024-08-06 12:57:03 +0000

    final

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,28 +1,44 @@
 import React, { useState } from "react";
-import Crear from "../component/crear";
+
 
 
 
 const TrafficLight = () => {
     const [color, setColor] = useState("red");
 
-    return (
-        <div className="contenedor">
-            <div className="palo"></div>
-            <div className="semaforo">
-                <div
-                    className={`light ${color === 'red' ? 'red activo' : 'red'}`}
-                    onClick={() => setColor('red')}
-                ></div>
-                <div className={`light ${color === 'yellow' ? 'yellow activo' : 'yellow'}`}
-                    onClick={() => setColor('yellow')}></div>
-                <div className={`light ${color === 'green' ? 'green activo' : 'green'}`}
-                    onClick={() => setColor('green')}></div>
-            </div>
-            <Crear
-            />
-        </div>
-    );
-}
+    const [nuevaLuz, setNuevaLuz] = useState(false);
+    const agregarNueva = () => {
+        setNuevaLuz(!nuevaLuz);
+ 
+    };
 
-export   default TrafficLight;
+    const getCambioColor = ()=>{
+        const color = ["red", "yellow", "green", "purple"];
+        const cambioColores = color[Math.floor(Math.random()*color.length)];
+        setColor(cambioColores);
+    }
+   
+        return (
+            <div className="contenedor">
+                <div className="palo"></div>
+                <div className="semaforo">
+                    <div
+                        className={`light ${color === 'red' ? 'red activo' : 'red'}`}
+                        onClick={() => setColor('red')}
+                    ></div>
+                    <div className={`light ${color === 'yellow' ? 'yellow activo' : 'yellow'}`}
+                        onClick={() => setColor('yellow')}></div>
+                    <div className={`light ${color === 'green' ? 'green activo' : 'green'}`}
+                        onClick={() => setColor('green')}></div>
+
+
+                    {nuevaLuz && (<div className={`light ${color === 'purple' ? 'purple activo' : 'purple'}`} onClick={() => setColor('purple')}></div>)}
+                </div>
+                <button type="button" onClick={() => agregarNueva(true)}>Luz morada</button>
+                <button type="button" onClick={() => setColor(getCambioColor)}>Alternas luces</button>
+
+            </div>
+        );
+    }
+
+    export default TrafficLight;
